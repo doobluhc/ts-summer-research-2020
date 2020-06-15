dac58c5b567c853de9ed623bf59fcd7a037611fb
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2020-06-15 11:55:09 -0400

    added average regret plots

--- stochastic_gradient_algorithm.jl
+++ stochastic_gradient_algorithm.jl
@@ -2,11 +2,13 @@ using DifferentialEquations
 using LinearAlgebra
 using PyPlot
 using PyCall
+using Statistics: mean
 @pyimport matplotlib.pyplot as pyplt
 
 
-function sample(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64;T = 1000)
+function sample(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64;T = 10000)
     cost = zeros(T)
+    regret = zeros(T)
     gain = zeros(T)
     avg = zeros(T)
     u = zeros(T)
@@ -26,6 +28,7 @@ function sample(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64;T = 1000
             x[t+1] = a₀ * x[t] + a₁ * x₀ + b₀ * u[t] + b₁ * u₀ + w
             gain[t] = -u[t]/x[t]
             cost[t] = x[t] * x[t]
+            regret[t] = cost[t]- 1.0
         else
             u[t] = -1/θ[t][3] * (θ[t][1]*x[t] + θ[t][2]*x[t-1] + θ[t][4]*u[t-1])
             ϕ[t] = [x[t];x[t-1];u[t];u[t-1]]
@@ -34,36 +37,56 @@ function sample(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64;T = 1000
             x[t+1] = a₀ * x[t] + a₁ * x[t-1] + b₀ * u[t] + b₁ * u[t-1] + w
             gain[t] = -u[t]/x[t]
             cost[t] = cost[t-1] + x[t] * x[t]
+            regret[t] = regret[t-1] + cost[t] - 1.0
         avg[t] = cost[t]/t
         θ[t+1] = θ[t] + ϕ[t]/r * (x[t+1] - transpose(ϕ[t]) * θ[t])
         end
     end
-    return avg,gain
+    return avg,gain,regret
 
 
 end
 
-function simulation(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64; T = 1000, N = 1)
+function simulation(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64; T = 10000, N = 1000)
     avg  = [ zeros(T) for n = 1:N ]
     gain = [ zeros(T) for n = 1:N ]
+    regret = [ zeros(T) for n = 1:N ]
+    avg_regret = zeros(T)
+    sum = 0.0
+    pyplt.clf()
 
     for n = 1:N
-        avg[n],gain[n]= sample(a₀,a₁,b₀,b₁; T=T)
+        avg[n],gain[n],regret[n]= sample(a₀,a₁,b₀,b₁; T=T)
 
-        pyplt.plot([t for t = 1:T],avg[n])
-        pyplt.axis([0,T,0,10])
-        pyplt.xlabel("t")
-        pyplt.ylabel("cost/t")
-        pyplt.title("cost function value/t vs t ")
-        pyplt.savefig("cost function value.png")
 
-        pyplt.plot([t for t = 1:T],gain[n])
-        pyplt.axis([0,T,0,10])
-        pyplt.xlabel("t")
-        pyplt.ylabel("gain")
-        pyplt.title("gain vs t ")
-        pyplt.savefig("gain.png")
+        # pyplt.plot([t for t = 1:T],avg[n])
+        # pyplt.axis([0,T,0,10])
+        # pyplt.xlabel("t")
+        # pyplt.ylabel("cost/t")
+        # pyplt.title("cost function value/t vs t ")
+        # pyplt.savefig("cost function value.png")
+
+        # pyplt.plot([t for t = 1:T],gain[n])
+        # pyplt.axis([0,T,0,10])
+        # pyplt.xlabel("t")
+        # pyplt.ylabel("gain")
+        # pyplt.title("gain vs t ")
+        # pyplt.savefig("gain.png")
 
     end
+
+    for t = 1:T
+        for n = 1:N
+            sum = sum + regret[n][t]
+        avg_regret[t] = log(abs(sum/N))
+        end
+    end
+
+    print(avg_regret)
+    pyplt.plot([log(t) for t = 1:T],avg_regret)
+    pyplt.xlabel("logt")
+    pyplt.ylabel("log(average regret)")
+    pyplt.title("log(average regret) vs log(t) ")
+    pyplt.savefig("average regret.png")
 
 end
