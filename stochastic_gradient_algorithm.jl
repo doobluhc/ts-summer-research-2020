ee826a54c9eb2fcaa66ed98a61e14feaf01a02c8
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2020-06-15 12:16:00 -0400

    modified code

--- stochastic_gradient_algorithm.jl
+++ stochastic_gradient_algorithm.jl
@@ -10,7 +10,7 @@ function sample(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64;T = 1000
     cost = zeros(T)
     regret = zeros(T)
     gain = zeros(T)
-    avg = zeros(T)
+    avg_cost = zeros(T)
     u = zeros(T)
     u₀ = 1
     x = zeros(T+1)
@@ -23,32 +23,39 @@ function sample(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64;T = 1000
         if t == 1
             u[t] = -1/θ[t][3] * (θ[t][1]*x[t] + θ[t][2]*x₀ + θ[t][4]*u₀)
             ϕ[t] = [x[t];x₀;u[t];u₀]
-            r = r + transpose(ϕ[t]) * ϕ[t]
+            r = r + ϕ'[t]* ϕ[t]
             w = randn()
             x[t+1] = a₀ * x[t] + a₁ * x₀ + b₀ * u[t] + b₁ * u₀ + w
             gain[t] = -u[t]/x[t]
             cost[t] = x[t] * x[t]
-            regret[t] = cost[t]- 1.0
         else
             u[t] = -1/θ[t][3] * (θ[t][1]*x[t] + θ[t][2]*x[t-1] + θ[t][4]*u[t-1])
             ϕ[t] = [x[t];x[t-1];u[t];u[t-1]]
-            r = r + transpose(ϕ[t]) * ϕ[t]
+            r = r + ϕ'[t] * ϕ[t]
             w = randn()
             x[t+1] = a₀ * x[t] + a₁ * x[t-1] + b₀ * u[t] + b₁ * u[t-1] + w
             gain[t] = -u[t]/x[t]
             cost[t] = cost[t-1] + x[t] * x[t]
-            regret[t] = regret[t-1] + cost[t] - 1.0
-        avg[t] = cost[t]/t
-        θ[t+1] = θ[t] + ϕ[t]/r * (x[t+1] - transpose(ϕ[t]) * θ[t])
+        avg_cost[t] = cost[t]/t
+        θ[t+1] = θ[t] + ϕ[t]/r * (x[t+1] - ϕ'[t] * θ[t])
         end
     end
-    return avg,gain,regret
 
+
+    for t in 1:T
+        if t == 1
+            regret[t] = cost[t] - avg_cost[T]
+        else
+            regret[t] = regret[t-1] + cost[t] - avg_cost[T]
+        end
+    end
+
+    return avg_cost,gain,regret
 
 end
 
 function simulation(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64; T = 10000, N = 1000)
-    avg  = [ zeros(T) for n = 1:N ]
+    avg_cost  = [ zeros(T) for n = 1:N ]
     gain = [ zeros(T) for n = 1:N ]
     regret = [ zeros(T) for n = 1:N ]
     avg_regret = zeros(T)
@@ -56,7 +63,7 @@ function simulation(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64; T =
     pyplt.clf()
 
     for n = 1:N
-        avg[n],gain[n],regret[n]= sample(a₀,a₁,b₀,b₁; T=T)
+        avg_cost[n],gain[n],regret[n]= sample(a₀,a₁,b₀,b₁; T=T)
 
 
         # pyplt.plot([t for t = 1:T],avg[n])
@@ -82,7 +89,6 @@ function simulation(a₀::Float64,a₁::Float64,b₀::Float64,b₁::Float64; T =
         end
     end
 
-    print(avg_regret)
     pyplt.plot([log(t) for t = 1:T],avg_regret)
     pyplt.xlabel("logt")
     pyplt.ylabel("log(average regret)")
