9ab586b228be2f4f385c6e15b494937bebc39174
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2020-06-16 23:00:02 -0400

    plotted the average regret for certainty equivalence

--- certainty_equivalence.jl
+++ certainty_equivalence.jl
@@ -1,12 +1,19 @@
 using PyPlot
 using PyCall
+using ControlSystems
+using LinearAlgebra
+using Plots
+using DataFrames
+using GLM
 @pyimport matplotlib.pyplot as pyplt
 
 
 function sample(a :: Float64, b :: Float64;T = 1000)
     cost = zeros(T)
+    regret = zeros(T)
     avg_cost = zeros(T)
     gain = zeros(T)
+    j_optimal = tr(dare(a,b,1.0,1.0))
 
     x = zeros(T+1)
     u = zeros(T)
@@ -28,8 +35,10 @@ function sample(a :: Float64, b :: Float64;T = 1000)
 
         if t == 1
             cost[t] = x[t]*x[t]
+            regret[t] = cost[t] - j_optimal
         else
             cost[t] = cost[t-1] + x[t]*x[t]
+            regret[t] = regret[t-1] + cost[t] - j_optimal
         end
 
         avg_cost[t] = cost[t]/t
@@ -39,7 +48,7 @@ function sample(a :: Float64, b :: Float64;T = 1000)
     end
 
 
-    return avg_cost,gain,â,b̂
+    return avg_cost,gain,â,b̂,regret
 
 end
 
@@ -48,14 +57,36 @@ function simulation(a :: Float64, b :: Float64;T = 1000, N = 100)
     gain  = [ zeros(T) for n = 1:N ]
     â = [ zeros(T) for n = 1:N ]
     b̂ = [ zeros(T) for n = 1:N ]
+    regret = [ zeros(T) for n = 1:N ]
+    sum = 0.0
+    avg_regret = zeros(T)
     pyplt.clf()
     for n = 1:N
-        avg_cost[n], gain[n],â[n], b̂[n] = sample(a,b)
-        pyplt.plot([t for t = 1:T],avg_cost[n])
-        pyplt.axis([0,T,0,100])
-        pyplt.xlabel("t")
-        pyplt.ylabel("cost/t")
-        pyplt.title("cost function value/t vs t ")
-        pyplt.savefig("average cost vs t.png")
+        avg_cost[n], gain[n],â[n], b̂[n], regret[n] = sample(a,b)
+
+        # pyplt.plot([t for t = 1:T],avg_cost[n])
+        # pyplt.axis([0,T,0,100])
+        # pyplt.xlabel("t")
+        # pyplt.ylabel("cost/t")
+        # pyplt.title("cost function value/t vs t ")
+        # pyplt.savefig("average cost vs t for CE.png")
     end
+
+    for t = 1:T
+        for n = 1:N
+            sum = sum + regret[n][t]
+        avg_regret[t] = log(abs(sum/N))
+        end
+    end
+
+    data = DataFrame(X=[log(t) for t = 1:T],Y = avg_regret)
+    best_fit_line = lm(@formula(Y~X),data)
+
+
+    # pyplt.scatter([log(t) for t = 1:T],avg_regret)
+    # pyplt.xlabel("logt")
+    # pyplt.ylabel("log(average regret)")
+    # pyplt.title("log(average regret) vs log(t) ")
+    # pyplt.savefig("average regret for CE.png")
+
 end
