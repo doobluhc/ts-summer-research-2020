34de5cbca9cc4c2c28ee3085b81ab51791710bc4
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2020-08-13 15:04:45 -0400

    Update ts_with_dynamic_episodes.jl

--- ts_with_dynamic_episodes.jl
+++ ts_with_dynamic_episodes.jl
@@ -26,8 +26,8 @@ function sample(A::Array{Float64},B::Array{Float64},Q::Array{Float64},R::Array{F
 
     S = dare(A,B,Q,R)
     j_optimal = tr(S)
-    println("optimal cost",j_optimal)
-    println("optimal gain" , (R + B'*S*B)\(B'*S*A))
+    # println("optimal cost",j_optimal)
+    # println("optimal gain" , (R + B'*S*B)\(B'*S*A))
     global_avg_cost = [zeros(T) for n = 1:N]
     global_cost = [zeros(T) for n = 1:N]
     global_regret = [zeros(T) for n = 1:N]
@@ -56,15 +56,11 @@ function sample(A::Array{Float64},B::Array{Float64},Q::Array{Float64},R::Array{F
             for i in 1:size(θ̂,2)
                 θ̃[:,i] = reshape(rand(MvNormal(θ̂[:,i],Σ)),p+q,1)
             end
-
             Â = reshape(θ̃[1:p,:],p,p)
-            B̂ = reshape(θ̂[(p+1):(p+q),:],p,q)
+            B̂ = reshape(θ̃[(p+1):(p+q),:],p,q)
             Ŝ = dare(Â,B̂,Q,R)
             Gⱼ = (R + B̂'*Ŝ*B̂)\(B̂'*Ŝ*Â)
-
             Σⱼ = Σ
-
-
             while t <= (tⱼ + Tⱼ₋₁) && det(Σ) >= 0.5*det(Σⱼ)
                 if t == 1
                     push!(cost,(x'*Q*x + u'*R*u)[1])
@@ -74,17 +70,14 @@ function sample(A::Array{Float64},B::Array{Float64},Q::Array{Float64},R::Array{F
                     push!(regret,regret[t-1]+(x'*Q*x + u'*R*u)[1] - j_optimal[1])
                 end
                 push!(avg_cost,cost[t]/t)
-
                 u = -Gⱼ * x
                 push!(gain,Gⱼ)
                 w = randn(p,1)
                 z = vcat(x,u)
                 x = A * x + B * u + w
-
                 normalize = 1 + (z' * Σ * z)[1]
                 θ̂ = θ̂ + (Σ * z * (x - θ̂'*z)') / normalize
                 Σ = Σ - Symmetric(Σ * z * z' * Σ) / normalize
-
                 t += 1
             end
             if t > T
@@ -100,19 +93,13 @@ function sample(A::Array{Float64},B::Array{Float64},Q::Array{Float64},R::Array{F
         sum_gain = sum_gain + global_gain[n][T]
     end
     avg_gain = N.\sum_gain
-    println("average estimated gain",avg_gain)
+    # println("average estimated gain",avg_gain)
 
     return global_cost, global_avg_cost, global_regret
 end
 function plot_avg_regret_vs_t(data,T)
-        avg_regret = zeros(T)
-        for t = 1:T
-            temp1 = zeros(N)
-            for n = 1:N
-                temp1[n] = data[n][t]
-            end
-            avg_regret[t] = mean(temp1)
-        end
+        data = reshape(hcat(data...),T,N)
+        avg_regret = reshape(mean(data,dims=2),T,)
         pyplt.clf()
         X = [t for t = 100:T]
         Y = avg_regret[100:T]
@@ -120,14 +107,15 @@ function plot_avg_regret_vs_t(data,T)
         p0 = [1.0,1.0]
         fit = curve_fit(model,X,Y,p0)
         y_fit = [fit.param[1] * x^fit.param[2] for x in X]
-        pyplt.plot(X, Y, color ="blue")
-        pyplt.plot(X, y_fit, color ="red")
-        pyplt.xlabel("t")
-        pyplt.ylabel("average regret")
-        pyplt.title("average regret vs t for TSDE")
-        pyplt.savefig("average regret vs t TSDEb.png")
-        println(fit.param[1])
-        println(fit.param[2])
+        # pyplt.plot(X, Y, color ="blue")
+        # pyplt.plot(X, y_fit, color ="red")
+        # pyplt.xlabel("t")
+        # pyplt.ylabel("average regret")
+        # pyplt.title("average regret vs t for TSDE")
+        # pyplt.savefig("average regret vs t TSDEb.png")
+        # println(fit.param[1])
+        # println(fit.param[2])
+        return fit.param[1],fit.param[2]
 end
 
 function plot_avg_cost_vs_t(data,T,N)
@@ -144,14 +132,8 @@ function plot_avg_cost_vs_t(data,T,N)
 end
 
 function plot_log_avg_regret_vs_log_t(data,T)
-    avg_regret = zeros(T)
-    for t = 1:T
-        temp1 = zeros(N)
-        for n = 1:N
-            temp1[n] = data[n][t]
-        end
-        avg_regret[t] = mean(temp1)
-    end
+    data = reshape(hcat(data...),T,N)
+    avg_regret = reshape(mean(data,dims=2),T,)
     pyplt.clf()
     X = reshape(log.([t for t = 100:T]),(T-99),1)
     Y = reshape(log.(avg_regret[100:T]),(T-99),1)
@@ -160,24 +142,20 @@ function plot_log_avg_regret_vs_log_t(data,T)
     y_pred = predict(regr,X)
     slope = float(regr.coef_)
 
-    pyplt.plot(X, Y, color ="blue")
-    pyplt.plot(X, y_pred, color ="red")
-    pyplt.xlabel("logt")
-    pyplt.ylabel("log(average regret)")
-    pyplt.title("log(average regret) vs log(t) for TSDE(slope = $slope)")
-    pyplt.savefig("log average regret vs log t TSDEb.png")
+    # pyplt.plot(X, Y, color ="blue")
+    # pyplt.plot(X, y_pred, color ="red")
+    # pyplt.xlabel("logt")
+    # pyplt.ylabel("log(average regret)")
+    # pyplt.title("log(average regret) vs log(t) for TSDE(slope = $slope)")
+    # pyplt.savefig("log average regret vs log t TSDEb.png")
+
+    return slope
 
 end
 
 function plot_avg_regret_vs_sqrt_t(data,T)
-    avg_regret = zeros(T)
-    for t = 1:T
-        temp1 = zeros(N)
-        for n = 1:N
-            temp1[n] = data[n][t]
-        end
-        avg_regret[t] = mean(temp1)
-    end
+    data = reshape(hcat(data...),T,N)
+    avg_regret = reshape(mean(data,dims=2),T,)
     pyplt.clf()
     X = reshape([sqrt(t) for t = 100:T],(T-99),1)
     Y = reshape(avg_regret[100:T],(T-99),1)
@@ -187,10 +165,43 @@ function plot_avg_regret_vs_sqrt_t(data,T)
     slope = float(regr.coef_)
     intercept = float(regr.intercept_)
     # pyplt.fill_between([sqrt(t) for t = 100:T],bot[100:T],top[100:T],color="gray")
-    pyplt.plot(X, Y, color ="blue")
-    pyplt.plot(X, y_pred, color ="red")
-    pyplt.xlabel("sqrtt")
-    pyplt.ylabel("average regret")
-    pyplt.title("average regret vs sqrt t for TSDE(slope = $slope)")
-    pyplt.savefig("average regret vs sqrt t TSDEb.png")
+    # pyplt.plot(X, Y, color ="blue")
+    # pyplt.plot(X, y_pred, color ="red")
+    # pyplt.xlabel("sqrtt")
+    # pyplt.ylabel("average regret")
+    # pyplt.title("average regret vs sqrt t for TSDE(slope = $slope)")
+    # pyplt.savefig("average regret vs sqrt t TSDEb.png")
+    return slope
+end
+
+function test()
+    I = 100
+    T = 10000
+    N = 10
+    p = 3
+    q = 3
+    c_list = []
+    α_list = []
+    Q = [1.0 0.0 0.0;0.0 1.0 0.0;0.0 0.0 1.0]
+    R = Q
+    log_slope_list = []
+    sqrt_slope_list = []
+    for i in 1:I
+        println(i)
+        M = randn(p,p)
+        A = M * M'
+        B = A
+        c = ctrb(A,B)
+        if rank(c) == p
+            data = sample(A,B,Q,R,T,N)
+            push!(log_slope_list,plot_log_avg_regret_vs_log_t(data[3],T))
+            push!(c_list,plot_avg_regret_vs_t(data[3],T)[1])
+            push!(α_list,plot_avg_regret_vs_t(data[3],T)[2])
+            push!(sqrt_slope_list,plot_avg_regret_vs_sqrt_t(data[3],T))
+        end
+
+    end
+
+    return c_list,α_list,log_slope_list,sqrt_slope_list
+
 end
